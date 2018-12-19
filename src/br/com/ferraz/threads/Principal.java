2b8203d53b025757743657043ee51026cd1addf4
Lucas Ferraz <carinamattos11@gmail.com>
2018-12-19 20:59:19 -0200

    First commit

---
+++ src/br/com/ferraz/threads/Principal.java
@@ -0,0 +1,13 @@
+package br.com.ferraz.threads;
+
+public class Principal {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Thread main");
+		
+		Thread.sleep(3000);
+		
+		System.out.println(1);
+	}
+
+}
---
+++ src/br/com/ferraz/threads/main/Teste.java
@@ -0,0 +1,25 @@
+package br.com.ferraz.threads.main;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Thread imprimeString = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println("Gravando no console......");
+				
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				System.out.println("Pronto!");
+			}
+		});
+		
+		imprimeString.start();
+	}
+	
+}
